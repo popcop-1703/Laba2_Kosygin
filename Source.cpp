66b6e8e7c1b9a96cda485c1232516c5fa3470afd
WamPan <markin.max.003@gmail.com>
2023-06-04 02:44:13 +0300

    .

--- Source.cpp
+++ Source.cpp
@@ -1,43 +1,69 @@
 #include <iostream>
 using namespace std;
-
-int main() {
-    int size;
-    cout << "Enter the size of the square: ";
-    cin >> size;
-
-    for (int i = 0; i < size; i++) {
-        for (int j = 0; j < size; j++) {
-            if (i == 0 || i == size - 1 || j == 0 || j == size - 1) {
-                cout << "*";
+void Brezenhem(char** z, int x0, int y0, int x1, int y1)
+{
+    int A, B, sign;
+    A = y1 - y0;
+    B = x0 - x1;
+    if (abs(A) > abs(B)) sign = 1;
+    else sign = -1;
+    int signa, signb;
+    if (A < 0) signa = -1;
+    else signa = 1;
+    if (B < 0) signb = -1;
+    else signb = 1;
+    int f = 0;
+    z[y0][x0] = '*';
+    int x = x0, y = y0;
+    if (sign == -1)
+    {
+        do {
+            f += A * signa;
+            if (f > 0)
+            {
+                f -= B * signb;
+                y += signa;
             }
-            else {
-                cout << " ";
+            x -= signb;
+            z[y][x] = '*';
+        } while (x != x1 || y != y1);
+    }
+    else
+    {
+        do {
+            f += B * signb;
+            if (f > 0) {
+                f -= A * signa;
+                x -= signb;
             }
-        }
+            y += signa;
+            z[y][x] = '*';
+        } while (x != x1 || y != y1);
+    }
+}
+int main()
+{
+    const int SIZE = 25; // размер поля
+    int x1, x2, y1, y2;
+    char** z;
+    z = new char* [SIZE];
+    for (int i = 0; i < SIZE; i++)
+    {
+        z[i] = new char[SIZE];
+        for (int j = 0; j < SIZE; j++)
+            z[i][j] = '-';
+    }
+    cout << "x1 = ";     cin >> x1;
+    cout << "y1 = ";     cin >> y1;
+    cout << "x2 = ";     cin >> x2;
+    cout << "y2 = ";    cin >> y2;
+    Brezenhem(z, x1, y1, x2, y2);
+    for (int i = 0; i < SIZE; i++)
+    {
+        for (int j = 0; j < SIZE; j++)
+            cout << z[i][j];
         cout << endl;
     }
-
-    cout << "Enter the character to fill the square with: ";
-    char fillChar;
-    cin >> fillChar;
-
-    for (int i = 0; i < size; i++) {
-        for (int j = 0; j < size; j++) {
-            if (i == 0 || i == size - 1 || j == 0 || j == size - 1) {
-                cout << "*";
-            }
-            else {
-                if (i == j || i + j == size - 1) {
-                    cout << fillChar;
-                }
-                else {
-                    cout << " ";
-                }
-            }
-        }
-        cout << endl;
-    }
-
+    cin.get(); cin.get();
     return 0;
 }
